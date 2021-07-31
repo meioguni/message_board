4d8d26f069a9427c155fb045b1a0578be0c95758
mei.oguni <oguni@primagest.co.jp>
2021-07-31 22:09:52 +0900

    Index/NewServletの修正

--- src/filters/EncodingFilter.java
+++ src/filters/EncodingFilter.java
@@ -16,33 +16,20 @@ import javax.servlet.annotation.WebFilter;
 @WebFilter("/*")
 public class EncodingFilter implements Filter {
 
-    /**
-     * Default constructor.
-     */
     public EncodingFilter() {
-        // TODO Auto-generated constructor stub
     }
 
     public void destroy() {
-        // TODO Auto-generated method stub
     }
 
-    /**
-     * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
-     */
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        // TODO Auto-generated method stub
-        // place your code here
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
 
-        // pass the request along the filter chain
         chain.doFilter(request, response);
     }
 
-    /**
-     * @see Filter#init(FilterConfig)
-     */
     public void init(FilterConfig fConfig) throws ServletException {
-        // TODO Auto-generated method stub
     }
 
 }
